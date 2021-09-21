e3d93ae30bbdca61b06daa53ccafafa004b047f9
Kevin Agoh <kevinagoh@hotmail.fr>
2021-09-21 23:51:27 +0200

    basic elements added to navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './navbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+function Navbar() {
+
+  const bell = <FontAwesomeIcon icon={faBell} />;
+  const searchIcon = <FontAwesomeIcon icon={faSearch} />;
+
+  const handleSearchBarToggle = () => {
+    const searchBar = document.querySelector('.search-bar');
+    searchBar.classList.toggle('visible');
+  }
+
+  return (
+    <div id='navbar'>
+      <div className="navbar-left">
+        <a href="#">
+          <img src="../assets/fake_logo_netflix.png" alt="Website logo" className='logo' />
+        </a>
+        <ul className='navbar-menu'>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#TV">TV Shows</a>
+          </li>
+          <li>
+            <a href="#movies">Movies</a>
+          </li>
+          <li>
+            <a href="#new">New & Popular</a>
+          </li>
+          <li>
+            <a href="#mist">My list</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <input type="text" className="search-bar" />
+        <span className="search-icon">
+          {searchIcon}
+        </span>
+        <span className="bell" onClick={handleSearchBarToggle}>
+          {bell}
+        </span>
+        <li>
+          <img src="" alt="" />
+          <div className="dropdown-menu">
+            <a href="#">Account</a>
+            <a href="#">Help Centre</a>
+            <a href="#">Sign Out</a>
+          </div>
+        </li>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
