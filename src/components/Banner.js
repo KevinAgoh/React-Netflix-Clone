4fae6ff481ecc5e4a2e982ca58a809d644529436
Kevin Agoh <kevinagoh@hotmail.fr>
2021-09-27 16:08:12 +0200

    done some styling on the banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import axios from "axios";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import './banner.scss';
 
 function Banner() {
@@ -8,6 +9,8 @@ function Banner() {
   const API_KEY = "3d27593948a5340e1af6efc162165567";
   const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}`;
   const axios = require('axios');
+  const playIcon = <FontAwesomeIcon icon={faPlay} />;
+  const infoIcon = <FontAwesomeIcon icon={faInfoCircle} />;
 
 
   const [movie, setMovie] = React.useState([]);
@@ -27,11 +30,13 @@ function Banner() {
 
   return (
     <header className='banner' style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`}}>
-      <h1 className="movie-title">{movie.name || movie.original_name}</h1>
-      <h3 className="movie-description">{movie.overview}</h3>
-      <div className="button-container">
-        <button></button>
-        <button></button>
+      <div className="info-container">
+        <h1 className="movie-title">{movie.title || movie.original_title}</h1>
+        <h3 className="movie-description">{movie.overview}</h3>
+        <div className="button-container">
+          <button className="play-btn">{playIcon} Play</button>
+          <button className="info-btn">{infoIcon} More Info</button>
+        </div>
       </div>
     </header>
   )
